8b4267591c714134bdd930fce832460e618eed72
nazaclaure <nazaret.claure@ucb.edu.bo>
2026-08-23 00:54:01 -0400

    feat: saludar por nombre

--- src/presenter.js
+++ src/presenter.js
@@ -1,9 +1,13 @@
+
 import saludar from "./saludador";
 
+const nombreInput = document.querySelector("#nombre");
 const form = document.querySelector("#saludador-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  div.innerHTML = "<p>" + saludar() + "</p>";
+
+  div.innerHTML = "<p>" + saludar(nombreInput.value) + "</p>";
+
 });
