30711acfefcba1bcfda67fcb7af43959e98706dd
nazaclaure <nazaret.claure@ucb.edu.bo>
2026-08-23 03:10:00 -0400

    feat: saludar en ingles

--- src/saludador.js
+++ src/saludador.js
@@ -1,13 +1,13 @@
 function saludar(nombre, hora, genero, edad, idioma = "es") {
   let saludo = "Hola";
-   let prefijo = "";
-   if (idioma === "es") {
+  let prefijo = "";
+  if (idioma === "es") {
     if (hora >= 6 && hora < 12) {
       saludo = "Buenos días";
-    } else if(hora >= 12 && hora < 19){
+    } else if (hora >= 12 && hora < 19) {
       saludo = "Buenas tardes";
-    } else{
-      saludo= "Buenas noches";
+    } else {
+      saludo = "Buenas noches";
     }
     if (edad > 30) {
       if (genero === "M") {
@@ -16,8 +16,22 @@ function saludar(nombre, hora, genero, edad, idioma = "es") {
         prefijo = "Sra. ";
       }
     }
-
-   }
+  } else if (idioma === "en") {
+    if (hora >= 6 && hora < 12) {
+      saludo = "Good morning";
+    } else if (hora >= 12 && hora < 19) {
+      saludo = "Good afternoon";
+    } else {
+      saludo = "Good evening";
+    }
+    if (edad > 30) {
+      if (genero === "M") {
+        prefijo = "Mr. ";
+      } else if (genero === "F") {
+        prefijo = "Mrs. ";
+      }
+    }
+  }
 
   return saludo + " " + prefijo + nombre;
 }
