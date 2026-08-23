f39b46c1aae2734ef0a8167e4b8b9e6816ce4849
nazaclaure <nazaret.claure@ucb.edu.bo>
2026-08-23 00:34:46 -0400

    feat: saludar con hola

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,9 @@
-import sumar from "./sumador";
+import saludar from "./saludador";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+const form = document.querySelector("#saludador-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
-
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = "<p>" + saludar() + "</p>";
 });
